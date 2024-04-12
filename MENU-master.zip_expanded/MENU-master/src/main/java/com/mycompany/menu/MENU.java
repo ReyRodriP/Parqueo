c9837e270cd87d473d4868a03b0c963ab4dfd760
ReyRodriP <reynaldorodriguez802@gmail.com>
2024-04-12 19:19:08 -0400

    Agregado

---
+++ MENU-master.zip_expanded/MENU-master/src/main/java/com/mycompany/menu/MENU.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.menu;
+
+/**
+ *
+ * @author el_bb
+ */
+public class MENU {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
