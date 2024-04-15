dd69a35e9890dd2d918f8fa334dbfa900db09033
ReyRodriP <reynaldorodriguez802@gmail.com>
2024-04-14 22:23:20 -0400

    Actualizacion hecha

--- MENU-master.zip_expanded/MENU-master/src/main/java/com/mycompany/menu/MENU.java
+++ MENU-master.zip_expanded/MENU-master/src/main/java/com/mycompany/menu/MENU.java
@@ -11,6 +11,7 @@ package com.mycompany.menu;
 public class MENU {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FormLogin objetoLogin = new FormLogin();
+        objetoLogin.setVisible(true);
     }
 }
